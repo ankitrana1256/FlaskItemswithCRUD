932c7f2205ca6e951c9489742cacdcf1e7593ddb
ankitrana1256 <76605774+ankitrana1256@users.noreply.github.com>
2022-01-11 22:02:32 +0530

    excel support added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import os
 from flask import Flask, request, render_template, redirect
 from flask.helpers import url_for
+import openpyxl
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -20,19 +22,41 @@ class products(db.Model):
 @app.route("/", methods=["POST", "GET"])
 def homepage():
     if (request.method) == "POST":
-        product = request.form["product"]
-        prod_image = request.form["prod_image"]
-        desc = request.form["desc"]
-        new_product = products(
-            product=product,
-            prod_image=prod_image,
-            desc=desc,
-        )
-        db.session.add(new_product)
-        db.session.commit()
-        return redirect(url_for("homepage"))
+        if request.form["submit"] == "submit":
+            product = request.form["product"]
+            prod_image = request.form["prod_image"]
+            desc = request.form["desc"]
+            new_product = products(
+                product=product,
+                prod_image=prod_image,
+                desc=desc,
+            )
+            db.session.add(new_product)
+            db.session.commit()
+            return redirect(url_for("homepage"))
+
     ProdList = products.query.all()
     return render_template("homepage.html", prods=ProdList)
+
+
+@app.route("/process", methods=["POST", "GET"])
+def process():
+    if request.method == "POST":
+        print("Here")
+        file = request.files["file"]
+        path = f"static/{file.filename}"
+        file.save(path)
+        wb = openpyxl.load_workbook(path)
+        ws = wb.active
+        max_data = ws.max_row
+        for i in range(2, max_data + 1):
+            itemname = ws.cell(column=1, row=i).value
+            description = ws.cell(column=2, row=i).value
+            url = ws.cell(column=3, row=i).value
+            new_prod = products(product=itemname, desc=description, prod_image=url)
+            db.session.add(new_prod)
+            db.session.commit()
+        return redirect(url_for("homepage"))
 
 
 @app.route("/delete/<int:id>")
